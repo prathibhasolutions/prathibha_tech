de7f9524ffc359f9acc349564413da78b9ebcfde
prathibhasolutions <prathibhasolutions1@gmail.com>
2026-01-01 10:10:13 +0530

    s

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u)qe*x7ru)yv%)xrwq-l1hk@&=)qw)c7$)jzsl&-ryzh+=@mm6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['65.0.89.209', 'localhost', '127.0.0.1', 'tech.prathibhasolutions.com', 'www.tech.prathibhasolutions.com']
 
 
 # Application definition
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
